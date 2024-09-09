900a2667e8139fd38a166c71114fc518903f3233
ledgerluminary55 <ledgerluminary55@gmail.com>
2024-09-09 11:26:59 +0300

    connect to capella and logging

--- api/index.js
+++ api/index.js
@@ -13,39 +13,44 @@ const log = Logger.child({
 });
 
 const main = async () => {
-
-await setupOttoman();
-
-app.use(cors(corsOptions));
-app.use(express.json()); // middleware to parse json
-app.use(cookieParser());
-
-// static route
-app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/', require('../routes/root'));
-
-// user routes - for testing
-app.use('/test', require('../routes/testRoutes'));
-
-// user routes - for /api/users and /api/user
-app.use('/api', require('../routes/userRoutes'));
-
-// user routes - for profiles
-app.use('/api/profiles', require('../routes/profileRoutes'));
-
-// article routes
-app.use('/api/articles', require('../routes/articleRoutes'));
-
-// tag route
-app.use('/api/tags', require('../routes/tagRoutes'));
-
-// comment routes
-app.use('/api/articles', require('../routes/commentRoutes'));
-
-app.listen(PORT, () => {
-    log.info(`Server running on port ${PORT}`);
-});
+    console.log("Starting Ottoman setup...");
+    await setupOttoman();
+    console.log("Ottoman setup complete.");
+  
+    console.log("Setting up middleware...");
+    app.use(cors(corsOptions));
+    app.use(express.json()); // middleware to parse json
+    app.use(cookieParser());
+  
+    // static route
+    app.use('/', express.static(path.join(__dirname, '/public')));
+    console.log("Static routes set up.");
+  
+    // user routes - for testing
+    app.use('/test', require('../routes/testRoutes'));
+    console.log("Test routes set up.");
+  
+    // user routes - for /api/users and /api/user
+    app.use('/api', require('../routes/userRoutes'));
+    console.log("User routes set up.");
+  
+    // user routes - for profiles
+    app.use('/api/profiles', require('../routes/profileRoutes'));
+    console.log("Profile routes set up.");
+  
+    // article routes
+    app.use('/api/articles', require('../routes/articleRoutes'));
+    console.log("Article routes set up.");
+  
+    // comment routes
+    app.use('/api/articles/:articleId/comments', require('../routes/commentRoutes'));
+    console.log("Comment routes set up.");
+  
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
 };
-
+  
 main();
+  
 module.exports = app;
--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,51 +1,56 @@
-const {model, registerGlobalPlugin, getDefaultInstance, Ottoman } = require('ottoman');
-const {userSchema, User} = require('../models/User');
-const {articleSchema, Article} = require('../models/Article');
-const {commentSchema, Comment} = require('../models/Comment');
-const  {Logger} = require('../config/logger');
+const { model, registerGlobalPlugin, getDefaultInstance, Ottoman } = require('ottoman');
+const { userSchema } = require('../models/User');
+const { articleSchema } = require('../models/Article');
+const { commentSchema } = require('../models/Comment');
+const { Logger } = require('../config/logger');
 const log = Logger.child({
-    namespace: 'DBConnect',
+  namespace: 'DBConnect',
 });
 
-const setupOttoman = async function(){
+const User = model('User', userSchema); 
+const Comment = model('Comment', commentSchema); 
+const Article = model('Article', articleSchema);
 
+const setupOttoman = async function () {
   // TODO: Fix global plugin registration
-    await registerGlobalPlugin((schema) => {
-        schema.pre('save', function (doc) {
-          log.info("SAAAAAVE");
-        });
-      });
+  await registerGlobalPlugin((schema) => {
+    schema.pre('save', function (doc) {
+      console.log("SAAAAAVE");
+    });
+  });
 
-    let ottoman = getDefaultInstance();
-    if (!ottoman) {
-      // if not exist default one, then create
-      ottoman = new Ottoman();
-    };
-  
-    const endpoint = process.env.DB_ENDPOINT || "couchbase://localhost";
-    const username = process.env.DB_USERNAME || "Administrator";
-    const password = process.env.DB_PASSWORD || "password";
-    const bucket = process.env.DB_BUCKET || "default";
-    const scope = process.env.DB_SCOPE || "_default";
+  let ottoman = getDefaultInstance();
+  if (!ottoman) {
+    ottoman = new Ottoman({ ensureIndexes: true });
+  }
 
-    try {
-      await ottoman.connect({
-        connectionString: endpoint,
-        username: username,
-        password: password,
-        bucketName: bucket,
-      });
-    } catch (e) {
-      throw(e);
-    }
+  const endpoint = process.env.DB_ENDPOINT || "couchbase://localhost";
+  const username = process.env.DB_USERNAME || "Administrator";
+  const password = process.env.DB_PASSWORD || "password";
+  const bucket = process.env.DB_BUCKET || "default";
+  const scope = process.env.DB_SCOPE || "_default";
 
-    const User = model('User', userSchema, { scopeName: scope });
-    const Comment = model('Comment', commentSchema, { scopeName: scope });
-    const Article = model('Article', articleSchema, { scopeName: scope });
+  try {
+    await ottoman.connect({
+      connectionString: endpoint,
+      username: username,
+      password: password,
+      bucketName: bucket,
+    });
+  } catch (e) {
+    throw e;
+  }
 
-    await ottoman.start();
-    log.info('Connected to Couchbase');
-}
+  try {
+    await ottoman.ensureIndexes();
+    console.log("Indexes ensured successfully");
+  } catch (indexError) {
+    log.error("Error ensuring indexes:", indexError);
+    throw indexError;
+  }
 
-module.exports = {setupOttoman, User, Comment, Article}
+  await ottoman.start();
+  console.log('Connected to Couchbase');
+};
 
+module.exports = { setupOttoman, User, Comment, Article };
